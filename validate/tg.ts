eaae057ddca02ffa536f1fd1f2ab64165d92cd14
mahatotarit <taritmahato1404@gmail.com>
2024-05-14 10:42:58 +0530

    telegram Conflict error fixed

--- validate/tg.ts
+++ validate/tg.ts
@@ -1,38 +1,43 @@
 import TelegramBot from 'node-telegram-bot-api';
 
-class SendTgMessage{
-   
-    public static prepare_message(prepare_vlaue: object | any): any{
-const messageText = `
+class SendTgMessage {
+
+  private static bot: TelegramBot | null = null;
+
+  public static prepare_message(prepare_vlaue: object | any): any {
+    const messageText = `
 <b>Network:</b> ${prepare_vlaue.network}\n
 <b>Tx Hash:</b> <code>${prepare_vlaue.hash}</code>\n
 <b>Amount:</b> <code>${prepare_vlaue.amount}</code>`;
 
-return messageText;
-    }
+    return messageText;
+  }
 
-    public static send_message = async(tg_value: object | any, message:string | any) =>{
+  public static send_message = async (
+    tg_value: object | any,
+    message: string | any,
+  ) => {
+    try {
 
-        const bot = new TelegramBot(tg_value.bot_token, {
+      if (!SendTgMessage.bot) {
+        SendTgMessage.bot = new TelegramBot(tg_value.bot_token, {
           polling: true,
         });
-
-        try {
-          bot
-            .sendMessage(tg_value.telegram_id, message, { parse_mode: 'HTML' })
-            .then(() => {
-              console.log('Message sent successfully');
-            })
-            .catch((error: Error) => {
-              console.error('Error:', error.message);
-            });
-        } catch (error:any) {
-           console.log(error.message);
-        }
+      }
 
 
+      SendTgMessage.bot
+        .sendMessage(tg_value.telegram_id, message, { parse_mode: 'HTML' })
+        .then(() => {
+          console.log('Message sent successfully');
+        })
+        .catch((error: Error) => {
+          console.error('Error:', error.message);
+        });
+    } catch (error: any) {
+      console.log(error.message);
     }
-
+  };
 }
 
 export {SendTgMessage};
